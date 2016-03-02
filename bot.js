412984966f381bd3f32c78a3fafee05b2ec98133
Borja Guzman <borja.guzman@rumbo.com>
2016-03-02 14:57:53 +0100

    Bot will have a conversation asking you for your search interests

--- bot.js
+++ bot.js
@@ -9,23 +9,77 @@ var opt = {
   url: 'http://api.stackexchange.com/2.2/search/advanced?order=desc&sort=activity&answers=1&site=stackoverflow',
   encoding: null
 };
+var opt2 = {
+  url: 'https://api.stackexchange.com/2.2/search/advanced?order=desc&sort=activity&accepted=true&site=stackoverflow',
+  encoding: null
+};
 
-botConfig.controller.hears(['hey (.*)'], ['direct_message', 'direct_mention', 'mention'], function(bot, message){
+var params = ['body', 'title'];
+
+botConfig.controller.hears(['hey (.*)'], ['direct_message', 'direct_mention', 'mention'], function( bot, message ){
   console.log(message);
-  var matches = message.text.match(/hey (.*)/i);
-  var name = matches[1];
-  requester.request( opt, function( err, answers ){
-    if( !err ){      
-      bot.reply( message, 'Hey ' + name + ', motafucka');
-      bot.reply( message, 'Got ' + answers.items.length + ' questions like yours, motafucka');
-      //bot.startConversation(message, (function()));
-    }else{
-      bot.reply( message, 'Not results, motafucka');
+  var index = 0;
+  var currentParams = {};
+  var savingParams; 
+  
+  var saveParams = function( paramKey ){
+    return function( paramValue ){
+      currentParams[paramKey] = paramValue;
     }
-  })
+  };
+  
+  var askParameter = function( response, bot ){
+    var currentParam = params[ index ];
+    index++;
+    if( currentParam !== undefined ){
+      bot.ask('Do you want to search by ' + currentParam + '? (y/n)', function(response, convbot){
+        if( response.text === 'y' || response.text === 'yes' ){
+          savingParams = saveParams( currentParam );
+          askParameterValue( bot, currentParam );
+        }else{
+          askParameter( response, convbot );
+        }
+        convbot.next();
+      });
+    }else{
+      makeRequest( bot, response, {
+        url: opt2.url + getParamString( currentParams ),
+        encoding: null
+      });
+    }
+  };
+  
+  var askParameterValue = function( bot, currentParam ){
+    bot.ask('Tell me the value for ' + currentParam, function(response, bot){
+      savingParams( response.text );
+      askParameter( response, bot );
+      bot.next();
+    });
+  };
+  
+  bot.startConversation(message, askParameter);
+  
 });
 
+var makeRequest = function( bot, message, options ){
+  var items;
+  requester.request( options, function( err, answers ){
+ console.log('this is err', arguments);
+    if( !err ){      
+      items = JSON.parse(answers).items;
+      console.log('this is items', items);
+      bot.say( 'Got ' + items.length + ' questions like yours, motafucka');
+    }else{
+      bot.say( 'Not results, motafucka');
+    }
+    bot.next();
+  })
+};
 
-var startConversation = function( bot, answers ){
-  return bot.startConversation
-}
+var getParamString = function( params ){
+  var str = '';;
+  for( var param in params ){
+    str += '&' + param + '=' + params[param].replace(/\s/g, '%20');
+  }
+  return str;
+};
