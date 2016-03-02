ac05857d8262f3f039ea2e570dccf8ec34a914d7
Borja Guzman <borja.guzman@rumbo.com>
2016-03-02 11:28:46 +0100

    Initial commit

---
+++ bot.js
@@ -0,0 +1,30 @@
+var requester = require('./lib/zlib.requester.js');
+var botHelper = require('./lib/bot.helper.js');
+var botConfig = botHelper( process.env.token );
+
+
+var opt = {
+  url: 'http://api.stackexchange.com/2.2/search/advanced?order=desc&sort=activity&answers=1&site=stackoverflow',
+  encoding: null
+}
+
+
+botConfig.controller.hears(['hey (.*)'], ['direct_message', 'direct_mention', 'mention'], function(bot, message){
+  console.log(message);
+  var matches = message.text.match(/hey (.*)/i);
+  var name = matches[1];
+  requester.request( opt, function( err, answers ){
+    if( !err ){      
+      bot.reply( message, 'Hey ' + name + ', motafucka');
+      bot.reply( message, 'Got ' + answers.items.length + ' questions like yours, motafucka');
+      //bot.startConversation(message, (function()));
+    }else{
+      bot.reply( message, 'Not results, motafucka');
+    }
+  })
+});
+
+
+var startConversation = function( bot, answers ){
+  return bot.startConversation
+}
