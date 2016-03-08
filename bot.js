a4d5ae5986cdef6fd2bc7c363eb49b836f2452cd
Borja Guzman <borja.guzman@rumbo.com>
2016-03-08 09:05:23 +0100

    Cleaned code

--- bot.js
+++ bot.js
@@ -5,19 +5,10 @@ var botRequester = require('./lib/bot.requester.js');
 var botConfig = botHelper( process.env.token );
 var botConversation = require('./lib/bot.conversation.js');
 
-var opt = {
-  url: 'https://api.stackexchange.com/2.2/search/advanced?order=desc&sort=activity&accepted=true&site=stackoverflow',
-  encoding: null
-};
-
-var params = ['body', 'title'];
-
 botConfig.controller.hears(
   ['hey (.*)'], 
   ['direct_message', 'direct_mention', 'mention'], 
-  function( bot, message ){
-    console.log(message);
-    
+  function( bot, message ){  
     
     var makeFinalRequest = function( bot, currentParams ){
       botRequester.makeRequest( currentParams, function( err, res ){
@@ -30,9 +21,6 @@ botConfig.controller.hears(
       });
     };
     
-    var index = 0;
-    var currentParams = {};
-    var savingParams; 
     bot.startConversation(message, function(err, convBot){
       var asker = new botConversation.ParameterAsker( convBot, makeFinalRequest );
       asker.askParameter();
